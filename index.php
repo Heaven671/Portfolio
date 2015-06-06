4e4bf6c33ec624eec681a2eeea3dfb277c5d849b
SchawnnDev <iperskill@gmail.com>
2015-06-06 19:05:44 +0200

    Changed index.php in index.html

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php $auth = 0; ?>
-<?php include 'lib/includes.php'; ?>
-<?php include 'partials/header.php'; ?>
-
-<div class="textecentre">
-	<h1>Bienvenue !</h1>
-</div>
-
-<?php include 'partials/footer.php'; ?>
