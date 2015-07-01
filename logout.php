9743031ae629cb1548f51d2afc97c450d4ad68bc
SchawnnDev <iperskill@gmail.com>
2015-07-01 21:34:06 +0200

    Change admin navbar & etc

--- logout.php
+++ logout.php
@@ -2,5 +2,6 @@
 $auth = 0;
 include('lib/includes.php');
 $_SESSION = array();
+setFlash('Tu es maintenant déconnecté !', 'success');
 header('Location:' . WEBROOT . 'index.php');
 ?>
--- partials/admin_header.php
+++ partials/admin_header.php
@@ -7,8 +7,40 @@
   <link href="<?= WEBROOT; ?>css/bootstrap.css" type='text/css' rel="stylesheet">
   <link href="<?= WEBROOT; ?>css/options.css" type='text/css' rel="stylesheet">
   <link href="<?= WEBROOT; ?>css/admin.css" type='text/css' rel="stylesheet">
+	<link href="<?= WEBROOT; ?>css/site.css" type='text/css' rel="stylesheet">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 </head>
+
+<body>
+  <nav class="nav navbar-nav">
+  <select class="select" onchange="location = this.options[this.selectIndex].value">
+    <option value="<?= WEBROOT; ?>admin/index.php">Accueil</option>
+    <option value="<?= WEBROOT; ?>admin/category.php" href="<?= WEBROOT; ?>admin/category.php">Catégories</option>
+    <option value="<?= WEBROOT; ?>admin/work.php" href="<?= WEBROOT; ?>admin/work.php">Réalisations</option>
+    <option value="<?= WEBROOT; ?>index.php" href="<?= WEBROOT; ?>index.php">Retour au site</option>
+    <option value="<?= WEBROOT; ?>logout.php" href="<?= WEBROOT; ?>logout.php">Déconnexion</option>
+  </select>
+    <ul>
+      <li>
+        <a class="nav" href="<?= WEBROOT; ?>admin/index.php">SchawnnDev</a>
+      </li>
+			<li>
+				<a class="nav" href="<?= WEBROOT; ?>admin/category.php">Catégories</a>
+			</li>
+      <li>
+        <a class="nav" href="<?= WEBROOT; ?>admin/work.php">Réalisations</a>
+      </li>
+      <li>
+        <a class="nav-right" href="<?= WEBROOT; ?>index.php">Retour au site</a>
+      </li>
+			<li>
+				<a class="nav-right" href="<?= WEBROOT; ?>logout.php">Se déconnecter</a>
+			</li>
+	  </ul>
+  </nav>
+</body>
+
+<!--
 
 <body>
 
@@ -29,11 +61,15 @@
           <li>
             <a href="<?= WEBROOT; ?>index.php" style="color: #C6E2FF">Retour au site</a>
           </li>
+					<li>
+            <a href="<?= WEBROOT; ?>logout.php" style="color: #C6E2FF">Se déconnecter</a>
+					</li>
         </ul>
     </div>
  </nav>
 <h1>&nbsp;</h1>
 </body>
 
+-->
 
 <?= flash(); ?>
