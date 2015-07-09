296c37a0732136a9e9b8f55d714542f08f8606db
SchawnnDev <SchawnnDev@users.noreply.github.com>
2015-07-09 22:43:08 +0200

    Removed useless code

--- partials/admin_header.php
+++ partials/admin_header.php
@@ -40,36 +40,4 @@
   </nav>
 </body>
 
-<!--
-
-<body>
-
-  <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-    <div class="container">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="index.php">SchawnnDev</a>
-      </div>
-        <ul class="nav navbar-nav">
-          <li>
-              <a href="category.php">Catégories</a>
-          </li>
-          <li>
-            <a href="work.php">Réalisations</a>
-          </li>
-        </ul>
-        <ul class="nav navbar-right">
-          <li>
-            <a href="<?= WEBROOT; ?>index.php" style="color: #C6E2FF">Retour au site</a>
-          </li>
-					<li>
-            <a href="<?= WEBROOT; ?>logout.php" style="color: #C6E2FF">Se déconnecter</a>
-					</li>
-        </ul>
-    </div>
- </nav>
-<h1>&nbsp;</h1>
-</body>
-
--->
-
 <?= flash(); ?>
